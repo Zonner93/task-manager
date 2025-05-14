08de07a35576486b23f8d54ca54929266954b92c
Adam Pawluczuk <zonner93@gmail.com>
2025-05-14 21:39:34 +0200

    add user exceptions and global exception handler

---
+++ src/main/java/com/example/task_manager/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.task_manager.exception.handler;
+
+import com.example.task_manager.exception.UserAlreadyExistsException;
+import com.example.task_manager.exception.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<String> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
